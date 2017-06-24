4e9ba5a824839ea31aed410f1f7325a3445f0e3a
1amBulletproof <brandon.tarney@gmail.com>
2017-06-24 13:41:57 -0400

    add value/getValue to hike enum

--- Homework_M6_605481/src/com/brandontarney/bookingrate/Rates.java
+++ Homework_M6_605481/src/com/brandontarney/bookingrate/Rates.java
@@ -2,27 +2,39 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.brandontarney.bookingrate;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-/** Rates is an object that provides a cost for a booked tour.  
- * There is a base rate and a premium rate that are used to calculate 
- * the cost of the tour.  Weekdays use the base rate, weekends use the
- * premium rate.
+/**
+ * Rates is an object that provides a cost for a booked tour. There is a base
+ * rate and a premium rate that are used to calculate the cost of the tour.
+ * Weekdays use the base rate, weekends use the premium rate.
  *
  * The premium rate is automatically generated as 1.5x the base rate
  *
  * @author evansrb1
  */
 public class Rates {
-    public static enum HIKE {GARDINER, HELLROARING, BEATEN};
+
+    public static enum HIKE {
+        GARDINER(0), HELLROARING(1), BEATEN(2);
+        private final int value;
+
+        private HIKE(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+    
     private HIKE hike;
     // flag to indicate total rate and day types need to be recalculated
-    private boolean synched=false;
+    private boolean synched = false;
     // weekday rate
     private int baseRate = -1;
     private int premiumRate = -1;
@@ -31,7 +43,7 @@ public class Rates {
     // start of trip
     private BookingDay beginDate = null;
     // end of trip
-    private BookingDay endDate = null;    
+    private BookingDay endDate = null;
     // Format the date to something readable
     private final static DateFormat FORMAT = DateFormat.getDateInstance();
     // cached number of weekend days
@@ -41,34 +53,34 @@ public class Rates {
 
     // we open June 1st
     private int seasonStartMonth = 6;
-    private int seasonStartDay = 1;    
+    private int seasonStartDay = 1;
 
     // we close Oct 1st.
     private int seasonEndMonth = 10;
-    private int seasonEndDay = 1;    
-    
+    private int seasonEndDay = 1;
+
     private String details = "none";
     private int[] validDurations = null;
 
-    public Rates(HIKE hike ) {
+    public Rates(HIKE hike) {
         this.hike = hike;
-        switch(hike) {
+        switch (hike) {
             case GARDINER:
-                baseRate=4000;
+                baseRate = 4000;
                 validDurations = new int[2];
                 validDurations[0] = 3;
                 validDurations[1] = 5;
                 break;
             case HELLROARING:
-                baseRate=3500;
-                
+                baseRate = 3500;
+
                 validDurations = new int[3];
                 validDurations[0] = 2;
                 validDurations[1] = 3;
                 validDurations[2] = 4;
                 break;
             case BEATEN:
-                baseRate=4500;                
+                baseRate = 4500;
                 validDurations = new int[2];
                 validDurations[0] = 5;
                 validDurations[1] = 7;
@@ -77,36 +89,41 @@ public class Rates {
         premiumRate = baseRate + (baseRate / 2);
     }
 
-    /** Get the total cost for the trip.  Returns -0.01 is something is amiss.
-     * 
+    /**
+     * Get the total cost for the trip. Returns -0.01 is something is amiss.
+     *
      * @return the cost of the trip
      */
     public double getCost() {
         if (synched) {
             return rate / 100.0;
         } else {
-            return calculateCost() / 100.0;            
+            return calculateCost() / 100.0;
         }
     }
 
-
-    /** Get the base rate for a weekday
-     @return the weekend rate
+    /**
+     * Get the base rate for a weekday
+     *
+     * @return the weekend rate
      */
     public double getBaseRate() {
-       return  baseRate / 100.0;
+        return baseRate / 100.0;
     }
 
-    /** Get the base rate for a weekend
-     @return the weekend rate
+    /**
+     * Get the base rate for a weekend
+     *
+     * @return the weekend rate
      */
     public double getPremiumRate() {
-       return  premiumRate / 100.0;
+        return premiumRate / 100.0;
     }
 
-
-    /** Set the starting day for the season.  This is used to validate booking
+    /**
+     * Set the starting day for the season. This is used to validate booking
      * dates. Default is June 1st.
+     *
      * @param month (1-Jan, 12-Dec)
      * @param day
      */
@@ -116,8 +133,10 @@ public class Rates {
         synched = false;
     }
 
-    /** Set the ending day for the season.  This is used to validate booking
+    /**
+     * Set the ending day for the season. This is used to validate booking
      * dates. Default is October 1st.
+     *
      * @param month (1-Jan, 12-Dec)
      * @param day
      */
@@ -127,25 +146,26 @@ public class Rates {
         synched = false;
     }
 
-    /** Determine if the entered dates are valid
-     * 
+    /**
+     * Determine if the entered dates are valid
+     *
      * @return true if both days exist and the begin date is before the end date
      */
-     public boolean isValidDates() {
-        if (beginDate == null ||
-                endDate == null) {
+    public boolean isValidDates() {
+        if (beginDate == null
+                || endDate == null) {
             details = "One of the dates was not defined";
             return false;
         } else if (beginDate.getYear() != endDate.getYear()) {
-                details = "The begin and end date must be within the same year";
-                return false;
+            details = "The begin and end date must be within the same year";
+            return false;
         } else if (beginDate.equals(endDate)) {
-                details = "The begin and end date must not be the same date";
-                return false;
+            details = "The begin and end date must not be the same date";
+            return false;
         } else if (beginDate.isValidDate() && endDate.isValidDate()) {
             if (!beginDate.after(endDate)) {
-                if ((!beginDate.before(seasonStartMonth, seasonStartDay)) &&
-                        (!endDate.after(seasonEndMonth, seasonEndDay))) {
+                if ((!beginDate.before(seasonStartMonth, seasonStartDay))
+                        && (!endDate.after(seasonEndMonth, seasonEndDay))) {
                     details = "valid dates";
                     return true;
                 } else {
@@ -160,54 +180,57 @@ public class Rates {
             details = "One of the dates was not a valid day";
             return false;
         }
-        
+
     }
 
-    /** Status of validation of data.  This is filled in once isValidDates() is called
-     * 
+    /**
+     * Status of validation of data. This is filled in once isValidDates() is
+     * called
+     *
      * @return A String indicating the status of the input data validity
      */
     public String getDetails() {
         return details;
     }
-    
+
     // calculate the cost, returns -1 if dates are bad
     // bug fix courtesy of Zachary Schmook! 8/7/2010
     private int calculateCost() {
-        if (! isValidDates() ) {
+        if (!isValidDates()) {
             return -1;
         }
         premiumDays = 0;
-        normalDays=0;
+        normalDays = 0;
 
         // Okay, if we got to here, the dates are somewhat sane...
         GregorianCalendar day = beginDate.getDate();
-        GregorianCalendar endDay = endDate.getDate();        
+        GregorianCalendar endDay = endDate.getDate();
         while (day.before(endDay)) {
             int dayOfWeek = day.get(Calendar.DAY_OF_WEEK);
-            if (dayOfWeek == Calendar.SATURDAY ||
-                    dayOfWeek == Calendar.SUNDAY ) {
+            if (dayOfWeek == Calendar.SATURDAY
+                    || dayOfWeek == Calendar.SUNDAY) {
                 premiumDays++;
             } else {
                 normalDays++;
             }
             day.add(Calendar.DAY_OF_WEEK, 1);
         }
-        if (endDay.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY ||
-                endDay.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+        if (endDay.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
+                || endDay.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
             premiumDays++;
         } else {
             normalDays++;
         }
-    //    System.out.println("normal days = " + normalDays + ", premium days = " + premiumDays);
+        //    System.out.println("normal days = " + normalDays + ", premium days = " + premiumDays);
         rate = normalDays * baseRate + premiumDays * premiumRate;
         synched = true;
-   //     System.out.println("baseRate = " + baseRate + ", premiumRate = " + premiumRate + ", rate = " + rate);
+        //     System.out.println("baseRate = " + baseRate + ", premiumRate = " + premiumRate + ", rate = " + rate);
         return rate;
     }
 
-    /** Get the number of weekdays in the reservation
-     * 
+    /**
+     * Get the number of weekdays in the reservation
+     *
      * @return number of weekdays in the reservation
      */
     public int getNormalDays() {
@@ -217,8 +240,9 @@ public class Rates {
         return normalDays;
     }
 
-    /** Get the number of weekend days in the reservation
-     * 
+    /**
+     * Get the number of weekend days in the reservation
+     *
      * @return number of weekend days in the reservation
      */
     public int getPremiumDays() {
@@ -228,7 +252,8 @@ public class Rates {
         return premiumDays;
     }
 
-    /** Get the beginning date of the reservation
+    /**
+     * Get the beginning date of the reservation
      *
      * @return a GregorianCalendar object of the beginning date
      */
@@ -239,7 +264,9 @@ public class Rates {
             return beginDate.getDate();
         }
     }
-    /** Get the end date of the reservation
+
+    /**
+     * Get the end date of the reservation
      *
      * @return a GregorianCalendar object of the end date
      */
@@ -251,23 +278,26 @@ public class Rates {
         }
     }
 
-    /** Get the beginning date of the reservation
+    /**
+     * Get the beginning date of the reservation
      *
      * @return a BookingDay object of the beginning date
      */
     public BookingDay getBeginBookingDay() {
-       return beginDate;
+        return beginDate;
     }
 
-    /** Get the end date of the reservation
+    /**
+     * Get the end date of the reservation
      *
      * @return a GregorianCalendar object of the end date
      */
     public BookingDay getEndBookingDay() {
-       return endDate;
+        return endDate;
     }
 
-    /** Set the beginning date of the reservation
+    /**
+     * Set the beginning date of the reservation
      *
      * @param beginDate the beginning date
      */
@@ -280,9 +310,10 @@ public class Rates {
     public int[] getDurations() {
         return validDurations;
     }
-    
-    /** Set the duration of the reservation.  One day hikes means no overnight,
-     * a two day hike is two days, one night.
+
+    /**
+     * Set the duration of the reservation. One day hikes means no overnight, a
+     * two day hike is two days, one night.
      *
      * @param int the duration of the hike
      */
@@ -303,34 +334,38 @@ public class Rates {
             endDate = new BookingDay(day.get(Calendar.YEAR),
                     day.get(Calendar.MONTH) + 1,
                     day.get(Calendar.DAY_OF_MONTH));
-            synched = false; 
+            synched = false;
             return true;
         }
     }
 
-    /** Set the end date of the reservation
-     * 
+    /**
+     * Set the end date of the reservation
+     *
      * @param endDate the end date
      */
     public void setEndDate(BookingDay endDate) {
         this.endDate = endDate;
         synched = false;
     }
-    
-    /** Get the hike type
-     * 
+
+    /**
+     * Get the hike type
+     *
      * @return HIKE enum
      */
     public HIKE getHike() {
         return this.hike;
     }
 
-    /** Quick test of the class */
+    /**
+     * Quick test of the class
+     */
     public static void main(String[] argv) {
-        BookingDay startDay = new BookingDay(2008,7,1);
-        BookingDay endDay = new BookingDay(2008,7, 7);
-        System.out.println("start Day of " + startDay + " " + (startDay.isValidDate()?"is valid":"is not valid"));
-        System.out.println("end Day " + endDay + " " + (endDay.isValidDate()?"is valid":"is not valid"));        
+        BookingDay startDay = new BookingDay(2008, 7, 1);
+        BookingDay endDay = new BookingDay(2008, 7, 7);
+        System.out.println("start Day of " + startDay + " " + (startDay.isValidDate() ? "is valid" : "is not valid"));
+        System.out.println("end Day " + endDay + " " + (endDay.isValidDate() ? "is valid" : "is not valid"));
 
         Rates rates = new Rates(HIKE.BEATEN);
 
@@ -341,7 +376,7 @@ public class Rates {
         if (rates.isValidDates()) {
             System.out.println("Cost of trip = " + rates.getCost());
             System.out.println("Weekdays: " + rates.getNormalDays());
-            System.out.println("Weekends: " + rates.getPremiumDays());        
+            System.out.println("Weekends: " + rates.getPremiumDays());
         } else {
             System.out.println("Sorry, but " + rates.getDetails());
         }
